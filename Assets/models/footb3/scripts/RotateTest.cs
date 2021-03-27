d3692b39ce84c0f42d361f2cfdaa41035c6a1cce
kuro <7445841+kuro7766@user.noreply.gitee.com>
2021-03-27 23:13:11 +0800

    add table football model

---
+++ Assets/models/footb3/scripts/RotateTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTest : MonoBehaviour
+{
+    private GameObject r;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GameObject.Find("abc");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(r.transform.position,Vector3.back,Time.deltaTime*500);
+        // transform.Rotate(Vector3.forward,Space.Self);
+    }
+
+}
