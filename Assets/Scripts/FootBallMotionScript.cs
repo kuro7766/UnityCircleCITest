e81d12ae78e75f00165cb0db87781b9438216e1e
kuro <7445841+kuro7766@user.noreply.gitee.com>
2021-04-04 00:11:25 +0800

    bouncing ball

--- Assets/Scripts/FootBallMotionScript.cs
+++ Assets/Scripts/FootBallMotionScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class FootBallMotionScript : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,6 +20,9 @@ public class FootBallMotionScript : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        // other.relativeVelocity.;
+        
+        _rigidbody.AddForce(other.impulse,ForceMode.Impulse);
+        // GetComponent<Rigidbody>().AddExplosionForce(other.relativeVelocity,ForceMode.Impulse);
+        
     }
 }
