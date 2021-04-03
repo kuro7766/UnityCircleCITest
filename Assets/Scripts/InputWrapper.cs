be4c7c4154e7ea400d4cab380b34578b22722157
kuro <7445841+kuro7766@user.noreply.gitee.com>
2021-04-03 17:02:42 +0800

    add football

---
+++ Assets/Scripts/InputWrapper.cs
@@ -0,0 +1,202 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+
+namespace InputWrapper
+{
+    public static class Input
+    {
+        static bool _touchSupported = _touchByFingerNotMouse();
+        // static bool touchSupported => UnityEngine.Input.touchSupported;
+
+        // static bool touchSupported => false;
+        static Touch? fakeTouch => SimulateTouchWithMouse.Instance.FakeTouch;
+        
+        private static bool _touchByFingerNotMouse()
+        {
+            var r = UnityEngine.Input.touchSupported;
+            switch (Application.platform)
+            {
+                case RuntimePlatform.Android:
+                    r = true;
+                    break;
+                case RuntimePlatform.WindowsEditor:
+                    r = false;
+                    break;
+                case RuntimePlatform.WindowsPlayer:
+                    r = false;
+                    break;
+                case RuntimePlatform.WebGLPlayer:
+                    r = false;
+                    break;
+            }
+
+            return r;
+        }
+
+		/*强制设置触摸类型*/
+        public static void UsingFinger(bool b)
+        {
+            _touchSupported = b;
+        }
+
+        public static bool GetButton(string buttonName)
+        {
+            return UnityEngine.Input.GetButton(buttonName);
+        }
+
+        public static bool GetButtonDown(string buttonName)
+        {
+            return UnityEngine.Input.GetButtonDown(buttonName);
+        }
+
+        public static bool GetButtonUp(string buttonName)
+        {
+            return UnityEngine.Input.GetButtonUp(buttonName);
+        }
+
+        public static bool GetMouseButton(int button)
+        {
+            return UnityEngine.Input.GetMouseButton(button);
+        }
+
+        public static bool GetMouseButtonDown(int button)
+        {
+            return UnityEngine.Input.GetMouseButtonDown(button);
+        }
+
+        public static bool GetMouseButtonUp(int button)
+        {
+            return UnityEngine.Input.GetMouseButtonUp(button);
+        }
+
+        public static int touchCount
+        {
+            get
+            {
+                if (_touchSupported)
+                {
+                    return UnityEngine.Input.touchCount;
+                }
+                else
+                {
+                    return fakeTouch.HasValue ? 1 : 0;
+                }
+            }
+        }
+
+        public static Touch GetTouch(int index)
+        {
+            if (_touchSupported)
+            {
+                return UnityEngine.Input.GetTouch(index);
+            }
+            else
+            {
+                Assert.IsTrue(fakeTouch.HasValue && index == 0);
+                return fakeTouch.Value;
+            }
+        }
+
+        public static Touch[] touches
+        {
+            get
+            {
+                if (_touchSupported)
+                {
+                    return UnityEngine.Input.touches;
+                }
+                else
+                {
+                    return fakeTouch.HasValue ? new[] {fakeTouch.Value} : new Touch[0];
+                }
+            }
+        }
+    }
+
+    internal class SimulateTouchWithMouse
+    {
+        static SimulateTouchWithMouse instance;
+        float lastUpdateTime;
+        Vector3 prevMousePos;
+        Touch? fakeTouch;
+
+
+        public static SimulateTouchWithMouse Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SimulateTouchWithMouse();
+                }
+
+                return instance;
+            }
+        }
+
+        public Touch? FakeTouch
+        {
+            get
+            {
+                update();
+                return fakeTouch;
+            }
+        }
+
+        void update()
+        {
+            if (Time.time != lastUpdateTime)
+            {
+                lastUpdateTime = Time.time;
+
+                var curMousePos = UnityEngine.Input.mousePosition;
+                var delta = curMousePos - prevMousePos;
+                prevMousePos = curMousePos;
+
+                fakeTouch = createTouch(getPhase(delta), delta);
+            }
+        }
+
+        static TouchPhase? getPhase(Vector3 delta)
+        {
+            if (UnityEngine.Input.GetMouseButtonDown(0))
+            {
+                return TouchPhase.Began;
+            }
+            else if (UnityEngine.Input.GetMouseButton(0))
+            {
+                return delta.sqrMagnitude < 0.01f ? TouchPhase.Stationary : TouchPhase.Moved;
+            }
+            else if (UnityEngine.Input.GetMouseButtonUp(0))
+            {
+                return TouchPhase.Ended;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        static Touch? createTouch(TouchPhase? phase, Vector3 delta)
+        {
+            if (!phase.HasValue)
+            {
+                return null;
+            }
+
+            var curMousePos = UnityEngine.Input.mousePosition;
+            return new Touch
+            {
+                phase = phase.Value,
+                type = TouchType.Indirect,
+                position = curMousePos,
+                rawPosition = curMousePos,
+                fingerId = 0,
+                tapCount = 1,
+                deltaTime = Time.deltaTime,
+                deltaPosition = delta
+            };
+        }
+    }
+}
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using MyUI.Reusable;
 using Unity.UIWidgets.material;
 using Unity.UIWidgets.painting;
 using Unity.UIWidgets.widgets;
 using UnityEngine;
+using Stack = Unity.UIWidgets.widgets.Stack;
 
 public class UI : MyApp
 {
     public override Widget getFlutterCode(BuildContext context)
     {
-        return new Center( child:new Text("Welcome to football game" ,style:new TextStyle(color: Colors.black
-        ,fontSize:50)));
+        return
+            new Stack(children: new List<Widget>
+            {
+                new Align(alignment: Alignment.topRight,child:
+                    new RaisedButton(onPressed:(() =>
+                        {
+                            
+                        }),child:new Text("menu")))
+                    ,
+                new Center(child: new Text("Welcome to football game", style: new TextStyle(color: Colors.black
+                    , fontSize: 50)))
+            });
+       
     }
 }
