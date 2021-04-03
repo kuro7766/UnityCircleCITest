672ef1cc193c1f8bc343c44f0c1d81363839e575
kuro <7445841+kuro7766@user.noreply.gitee.com>
2021-04-03 23:57:09 +0800

    add gesture logic

--- Assets/Scripts/FootBallMotionScript.cs
+++ Assets/Scripts/FootBallMotionScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,4 +16,6 @@ public class FootBallMotionScript : MonoBehaviour
     {
         
     }
+
+    
 }
